7cc7e2421c706a01a44c84fae19997b4c4dcb4a7
Gonzalo Bertolucci <114008639+GonzaBertolucci@users.noreply.github.com>
2024-09-25 20:39:47 -0300

    Ejercicio n3 resuelto CuentaBancaria POO
    Crea una clase llamada CuentaBancaria que represente una cuenta bancaria. La clase debe tener los siguientes atributos:
    Número de cuenta (entero)
    Saldo actual (float)
    Implementa los siguientes métodos:
    Un constructor que me permita establecer el número de cuenta y el saldo.
    Un método depositar(float monto) que incremente el saldo.
    Un método retirar(float monto) que disminuya el saldo si hay fondos suficientes, caso contrario no hace nada.
    Un método obtenerSaldo() que devuelva el saldo actual.

---
+++ CuentaBancaria/CuentaBancaria.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include "CuentaBancaria.h"
+using namespace std;
+
+CuentaBancaria::CuentaBancaria(int numeroDeCuenta ,float saldo){
+    _saldoActual = saldo;
+    _numeroDeCuenta = numeroDeCuenta;
+}
+
+void CuentaBancaria::depositar(){
+    float auxDeposito;
+    cout<<"Ingrese el dinero a depositar: ";
+    cin>>auxDeposito;
+    _saldoActual += auxDeposito;
+}
+
+void CuentaBancaria::retirar(){
+    float auxRetiro;
+    if(_saldoActual > 0){
+        _saldoActual -= auxRetiro;
+    }
+}
+
+
+
